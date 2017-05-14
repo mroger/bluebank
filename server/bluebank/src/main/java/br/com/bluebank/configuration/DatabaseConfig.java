b569a0f42ab862aab15bf1006fee97991a9df481
Marcos R Oliveira <mroger.oliveira@gmail.com>
2017-05-14 09:49:04 -0300

    Adding script to create tables and relationships Adding script to populate tables with accounts and account holders Adding class to configure embedded HSQL database and execute database scripts

---
+++ server/bluebank/src/main/java/br/com/bluebank/configuration/DatabaseConfig.java
@@ -0,0 +1,26 @@
+package br.com.bluebank.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class DatabaseConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder
+			.setType(EmbeddedDatabaseType.HSQL)
+			.addScript("db/create-db.sql")
+			.addScript("db/populate-db.sql")
+			.build();
+		return db;
+	}
+	
+}
---
+++ server/bluebank/src/main/resources/db/populate-db.sql
@@ -0,0 +1,11 @@
+INSERT INTO ACCOUNT (number, agency, balance) VALUES (1, 1, 100.0);
+INSERT INTO ACCOUNT (number, agency, balance) VALUES (2, 1, 150.0);
+INSERT INTO ACCOUNT (number, agency, balance) VALUES (1, 2, 500.0);
+INSERT INTO ACCOUNT (number, agency, balance) VALUES (2, 2, 700.0);
+
+INSERT INTO ACCOUNT_HOLDER (cpf, account_number, account_agency, name, creation_date) VALUES ('51271204452', 1, 1, 'Charlie', '2017-05-14 10:00:00');
+INSERT INTO ACCOUNT_HOLDER (cpf, account_number, account_agency, name, creation_date) VALUES ('51271204452', 1, 1, 'Charlie', '2017-05-14 10:00:00');
+INSERT INTO ACCOUNT_HOLDER (cpf, account_number, account_agency, name, creation_date) VALUES ('59337602180', 2, 1, 'Jake', '2017-05-14 10:00:00');
+INSERT INTO ACCOUNT_HOLDER (cpf, account_number, account_agency, name, creation_date) VALUES ('43435392100', 1, 2, 'Alan', '2017-05-14 10:00:00');
+INSERT INTO ACCOUNT_HOLDER (cpf, account_number, account_agency, name, creation_date) VALUES ('84061237020', 2, 2, 'Berta', '2017-05-14 10:00:00');
+INSERT INTO ACCOUNT_HOLDER (cpf, account_number, account_agency, name, creation_date) VALUES ('82355099979', 1, 1, 'Rose', '2017-05-14 10:00:00');
