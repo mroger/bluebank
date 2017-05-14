6b9d441202c22aa70985b4f39c422c2275ab298d
Marcos R Oliveira <mroger.oliveira@gmail.com>
2017-05-14 10:13:51 -0300

    Placing application class in a better package

--- server/bluebank/src/main/java/br/com/bluebank/BluebankApplication.java
+++ server/bluebank/src/main/java/br/com/bluebank/BluebankApplication.java
@@ -1,4 +1,4 @@
-package br.com.bluebank.application;
+package br.com.bluebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
