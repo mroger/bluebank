735ee1e36b64a413c2fa32cc42def684ae6c983f
Marcos R Oliveira <mroger.oliveira@gmail.com>
2017-05-15 00:38:29 -0300

    Parameterizing CORS origin[C

--- server/bluebank/src/main/java/br/com/bluebank/filter/CorsFilter.java
+++ server/bluebank/src/main/java/br/com/bluebank/filter/CorsFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -25,14 +26,13 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class CorsFilter implements Filter {
 	
-	//TODO Inject value
-	//@Value("${schedule.alloworigin}")
-	//private String allowOrigin;
+	@Value("${bluebank.alloworigin}")
+	private String allowOrigin;
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
+		response.setHeader("Access-Control-Allow-Origin", allowOrigin);
 		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
 		response.setHeader("Access-Control-Max-Age", "3600");
